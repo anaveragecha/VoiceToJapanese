a25484680087e5ecf1360bd805640b9e416bcc49
anaveragecha <mechbuilderbeast123@gmail.com>
2023-06-03 23:16:46 -0500

    Removed the need for an OPENAI API, now works with local oobabooga's api.

--- chatbot.py
+++ chatbot.py
@@ -1,18 +1,21 @@
 import os
 import re
 import traceback
-import openai
+import requests
+import json
 import STTSLocal as STTS
 
+HOST = 'localhost:5000'
+URI = f'http://{HOST}/api/v1/chat'
 
-openai_api_key = ''
 AI_RESPONSE_FILENAME = 'ai-response.txt'
 character_limit = 3000
 
 lore = ''
 try:
     with open('./lore.txt', 'r', encoding='utf-8') as file:
-        lore = file.read()
+        #lore = file.read()
+        lore = 'Name\n' #not needed
 except Exception:
     print("error when reading lore.txt")
     print(traceback.format_exc())
@@ -25,45 +28,73 @@ message_log = [
 
 logging_eventhandlers = []
 
+history = {'internal': [], 'visible': []}
 
 def send_user_input(user_input):
-    log_message(f'user: {user_input}')
-    global message_log
-    global openai_api_key
-    if (openai_api_key == ''):
-        openai_api_key = os.getenv("OPENAI_API_KEY")
-
-    openai.api_key = openai_api_key
-    print(f"Sending: {user_input}")
     message_log.append({"role": "user", "content": user_input})
-    print(message_log)
-    total_characters = sum(len(message['content']) for message in message_log)
-    print(f"total_characters: {total_characters}")
-    while total_characters > character_limit and len(message_log) > 1:
-        print(
-            f"total_characters {total_characters} exceed limit of {character_limit}, removing oldest message")
-        total_characters -= len(message_log[1]["content"])
-        message_log.pop(1)
+    log_message(f'user: {user_input}')
 
-    response = None
-    try:
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=message_log
-        )
-    except Exception:
-        log_message("Error when loading api key from environment variable")
-        log_message(
-            "You need an API key from https://platform.openai.com/ stored in an environment variable with name \"OPENAI_API_KEY\" to use the chat feature")
-        print(traceback.format_exc())
-        return
-    text_response = response['choices'][0]['message']['content']
-    message_log.append({"role": "assistant", "content": text_response})
-    log_message(f'AI: {text_response}')
-    with open(AI_RESPONSE_FILENAME, "w", encoding="utf-8") as file:
-        separated_text = separate_sentences(text_response)
-        file.write(separated_text)
-    STTS.start_TTS_pipeline(text_response)
+    request = {
+        'user_input': user_input,
+        'history': history,
+        'mode': 'chat',  # Valid options: 'chat', 'chat-instruct', 'instruct'
+        'character': 'Your character', # your character from the oobabooga text-gen repo
+        'instruction_template': 'None',
+        'your_name': 'You',
+
+        'regenerate': False,
+        '_continue': False,
+        'stop_at_newline': False,
+        'chat_prompt_size': 2048,
+        'chat_generation_attempts': 1,
+        'chat-instruct_command': 'Continue the chat dialogue below. Write a single reply for the character "".\n\n',
+
+        'max_new_tokens': 200,
+        'do_sample': True,
+        'temperature': 0.6,
+        'top_p': 0.9,
+        'typical_p': 1,
+        'epsilon_cutoff': 0,  # In units of 1e-4
+        'eta_cutoff': 0,  # In units of 1e-4
+        'tfs': 1,
+        'top_a': 0,
+        'repetition_penalty': 1.15,
+        'top_k': 100,
+        'min_length': 0,
+        'no_repeat_ngram_size': 0,
+        'num_beams': 1,
+        'penalty_alpha': 0,
+        'length_penalty': 1,
+        'early_stopping': False,
+        'mirostat_mode': 0,
+        'mirostat_tau': 5,
+        'mirostat_eta': 0.1,
+        'seed': -1,
+        'add_bos_token': True,
+        'truncation_length': 2048,
+        'ban_eos_token': False,
+        'skip_special_tokens': True,
+        'stopping_strings': []
+    }
+
+    response = requests.post(URI, json=request)
+
+    if response.status_code == 200:
+        result = response.json()['results'][0]['history']
+        
+        text_response = result['visible'][-1][1]
+        message_log.append({"role": "assistant", "content": text_response})
+        log_message(f'AI: {text_response}')
+
+        with open(AI_RESPONSE_FILENAME, "w", encoding="utf-8") as file:
+            separated_text = separate_sentences(text_response)
+            file.write(separated_text)
+
+        STTS.start_TTS_pipeline(text_response)
+
+        return text_response
+    else:
+        return None
 
 
 def log_message(message_text):
